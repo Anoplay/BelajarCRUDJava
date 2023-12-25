bf87060d475d0426019247c2536d03ea731c5862
Anoplay <agungprasetyo1111@gmail.com>
2023-12-25 13:01:15 +0700

    Belajar CRUD Java + MYSQL

---
+++ src/belajarcrud/BelajarCRUD.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package belajarcrud;
+import java.sql.*;
+
+import net.proteanit.sql.DbUtils;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author agung
+ */
+public class BelajarCRUD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
